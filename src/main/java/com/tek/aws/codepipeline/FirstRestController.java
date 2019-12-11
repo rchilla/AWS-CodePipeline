1c0ff74a9ce3f2897de3debb9294cd5a0c7228a0
Ramesh Chilla <ramesh.chilla@gmail.com>
2019-12-11 16:04:29 +0530

    Commit-2

--- src/main/java/com/tek/aws/codepipeline/FirstRestController.java
+++ src/main/java/com/tek/aws/codepipeline/FirstRestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstRestController {
 	public String awsCodeipeline() {
+		
 		return "Introduction of AWS Codepioeline";
 	}
 }
