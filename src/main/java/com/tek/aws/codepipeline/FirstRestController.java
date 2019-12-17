93a1250ea133d257d8a4cdc5b0c35a4333f8b567
Ramesh Chilla <ramesh.chilla@gmail.com>
2019-12-17 15:46:47 +0530

    Test for SNS-11

--- src/main/java/com/tek/aws/codepipeline/FirstRestController.java
+++ src/main/java/com/tek/aws/codepipeline/FirstRestController.java
@@ -8,7 +8,7 @@ public class FirstRestController {
 	public String awsCodeipeline() {
 		
 		return "Introduction of AWS Codepioeline";
-		//System.out.println("This is to test");
+		System.out.println("This is to test");
 
 		
 	}
