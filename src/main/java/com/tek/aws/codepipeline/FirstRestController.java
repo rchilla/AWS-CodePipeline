42c3aff739d2a902ca18fabde23b1cc68f223539
Ramesh Chilla <ramesh.chilla@gmail.com>
2019-12-20 12:19:12 +0530

    First Commit

--- src/main/java/com/tek/aws/codepipeline/FirstRestController.java
+++ src/main/java/com/tek/aws/codepipeline/FirstRestController.java
@@ -1,14 +1,17 @@
 package com.tek.aws.codepipeline;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/awsCodepipeline") 
 @RestController
 public class FirstRestController {
+	
+	@GetMapping
 	public String awsCodeipeline() {
 		
 		return "Introduction of AWS Codepioeline";
-		System.out.println("This is to test");
+		//System.out.println("This is to test");
 
 		
 	}
